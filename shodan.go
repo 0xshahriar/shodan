e675f8d402eeedc18872796df0574e6ec4e3537a
Md. Shahriar Alam Shaon <sadjoker.localhost@gmail.com>
2023-01-19 22:32:37 +0600

    Update shodan.go

--- shodan.go
+++ shodan.go
@@ -1,42 +1,30 @@
 package main
 
 import (
-
     "fmt"
-
     "os"
-
     "github.com/shodanhq/shodan-go"
-
 )
 
 func main() {
-
     // Get the Shodan API key from the environment variable
-
     apiKey := os.Getenv("SHODAN_API_KEY")
 
     // Create a new Shodan client
-
     client := shodan.NewClient(apiKey)
 
+    // Get the search query from the user input
+    fmt.Print("Enter search query: ")
+    var query string
+    fmt.Scanln(&query)
+
     // Search for the given query
-
-    query := "example query"
-
     results, err := client.Search(query)
-
     if err != nil {
-
         fmt.Println(err)
-
         return
-
     }
 
     // Print the results
-
     fmt.Println(results)
-
 }
-
